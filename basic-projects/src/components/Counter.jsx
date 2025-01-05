70e78e307fe40f2f530dacd3a4f57cafcce77b22
soumadip-dev <soumadipmajila@gmail.com>
2025-01-05 20:08:38 +0530

    feat: add basic counter project using React

---
+++ basic-projects/src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import '../style.css';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <div>
+      <div className="container">
+        <h1 className="number">{count}</h1>
+      </div>
+      <div className="btns-container">
+        <button className="increment" onClick={increment}>
+          +
+        </button>
+        <button className="increment" onClick={decrement}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
