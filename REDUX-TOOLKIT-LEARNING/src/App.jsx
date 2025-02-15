224e854c262a19ef132cbdcb80d911af8394db4c
soumadip-dev <soumadipmajila@gmail.com>
2025-02-15 12:00:00 +0530

    feat: learn and implement createAsyncThunk to fetch data from API

--- REDUX-TOOLKIT-LEARNING/src/App.jsx
+++ REDUX-TOOLKIT-LEARNING/src/App.jsx
@@ -2,15 +2,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import Counter from './components/Counter';
 import Stats from './components/Stats';
 import { decrement, increment } from './features/counters/counterSlice';
+import Posts from './components/Posts';
 
 const App = () => {
   const counters = useSelector(state => state.counters);
-
   const dispatch = useDispatch();
 
   const handleIncrement = id => {
     dispatch(increment(id));
   };
+
   const handleDecrement = id => {
     dispatch(decrement(id));
   };
@@ -18,19 +19,23 @@ const App = () => {
   const totalCount = counters.reduce((sum, current) => sum + current.value, 0);
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Simple Counter App</h1>
+    <div className="min-h-screen w-full bg-gray-100 py-10 px-4">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">Simple Counter App</h1>
 
-      <div className="flex flex-col items-center gap-4 mb-4">
-        {counters.map(counter => (
-          <Counter
-            key={counter.id}
-            count={counter.value}
-            onIncrement={() => handleIncrement(counter.id)}
-            onDecrement={() => handleDecrement(counter.id)}
-          />
-        ))}
-        <Stats count={totalCount} />
+        <div className="flex flex-col items-center gap-6 mb-10">
+          {counters.map(counter => (
+            <Counter
+              key={counter.id}
+              count={counter.value}
+              onIncrement={() => handleIncrement(counter.id)}
+              onDecrement={() => handleDecrement(counter.id)}
+            />
+          ))}
+          <Stats count={totalCount} />
+        </div>
+
+        <Posts />
       </div>
     </div>
   );
--- REDUX-TOOLKIT-LEARNING/src/app/store.js
+++ REDUX-TOOLKIT-LEARNING/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counters/counterSlice.js';
+import postsReducer from '../features/posts/postSlice.js';
 
 const store = configureStore({
   reducer: {
     counters: counterReducer,
+    posts: postsReducer,
   },
 });
 
