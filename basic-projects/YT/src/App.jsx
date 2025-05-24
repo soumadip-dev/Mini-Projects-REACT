647a520e07643cf43d04367e8433a69c1b19307f
soumadip-dev <soumadipmajila@gmail.com>
2025-05-24 23:43:42 +0530

    Learn React Children(Special Prop)

--- basic-projects/YT/src/App.jsx
+++ basic-projects/YT/src/App.jsx
@@ -2,16 +2,31 @@
 // import Todo from './components/Todo';
 // import Meals from './components/Meals';
 // import Calculator from './components/Calculator';
-import CopyInput from './components/CopyInput';
+// import CmpA from './compo/cmpA';
+import UnderStandChildren from './components/underStandChildren';
 
 const App = () => {
+  const name = 'Soumadip Majila';
   return (
     <div>
       {/* <Counter /> */}
       {/* <Todo /> */}
       {/* <Meals /> */}
       {/* <Calculator /> */}
-      <CopyInput />
+      {/* <CmpA name={name} /> */}
+      <UnderStandChildren>
+        <div style={{ color: 'green' }}>
+          What do you want to post <br />
+          <input type="text" />
+        </div>
+      </UnderStandChildren>
+
+      <UnderStandChildren>
+        <div style={{ color: 'red' }}>
+          What is your favorite color? <br />
+          <input type="color" />
+        </div>
+      </UnderStandChildren>
     </div>
   );
 };
---
+++ basic-projects/YT/src/components/UnderStandChildren.jsx
@@ -0,0 +1,16 @@
+const UnderStandChildren = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'white',
+        borderRadius: 10,
+        color: 'black',
+        padding: 10,
+        margin: 10,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+export default UnderStandChildren;
