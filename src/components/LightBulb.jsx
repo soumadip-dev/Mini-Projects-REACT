4495c23ff619fcd050bcc9879678e784ba641bbe
soumadip-dev <soumadipmajila@gmail.com>
2025-05-25 21:12:56 +0530

    Refactor: Combine bulbOn and setBulbOn into a single BulbContext object

--- src/components/LightBulb.jsx
+++ src/components/LightBulb.jsx
@@ -1,16 +1,13 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 const BulbContext = createContext();
-const SetBulbContext = createContext();
 
 const LightBulb = () => {
   const [bulbOn, setBulbOn] = useState(true);
   return (
-    <BulbContext.Provider value={bulbOn}>
-      <SetBulbContext.Provider value={setBulbOn}>
-        <BulbState />
-        <ToggleBulbState />
-      </SetBulbContext.Provider>
+    <BulbContext.Provider value={{ bulbOn, setBulbOn }}>
+      <BulbState />
+      <ToggleBulbState />
     </BulbContext.Provider>
   );
 };
@@ -19,7 +16,7 @@ export default LightBulb;
 function BulbState() {
   return (
     <BulbContext.Consumer>
-      {bulbOn => <div>{bulbOn ? 'Bulb on' : 'Bulb off'}</div>}
+      {({ bulbOn }) => <div>{bulbOn ? 'Bulb on' : 'Bulb off'}</div>}
     </BulbContext.Consumer>
   );
 }
@@ -27,14 +24,12 @@ function BulbState() {
 function ToggleBulbState() {
   return (
     <BulbContext.Consumer>
-      {bulbOn => (
-        <SetBulbContext.Consumer>
-          {setBulbOn => {
-            const toggle = () => setBulbOn(!bulbOn);
-            return <button onClick={toggle}>Toggle the bulb</button>;
-          }}
-        </SetBulbContext.Consumer>
-      )}
+      {({ bulbOn, setBulbOn }) => {
+        {
+          const toggle = () => setBulbOn(!bulbOn);
+          return <button onClick={toggle}>Toggle the bulb</button>;
+        }
+      }}
     </BulbContext.Consumer>
   );
 }
