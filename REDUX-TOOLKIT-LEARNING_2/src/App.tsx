80c6fb664ae074a8bb27e6183da6f6561284f353
soumadip-dev <soumadipmajila@gmail.com>
2025-10-02 14:35:43 +0530

    feat: add Material UI for component styling

--- REDUX-TOOLKIT-LEARNING_2/src/App.tsx
+++ REDUX-TOOLKIT-LEARNING_2/src/App.tsx
@@ -1,9 +1,13 @@
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { Container, Typography } from '@mui/material';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Container maxWidth="md">
+        <Typography variant="h1">Hello</Typography>
+      </Container>
       <div>App</div>
     </Provider>
   );
