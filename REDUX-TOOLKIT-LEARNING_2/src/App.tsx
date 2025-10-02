7fe6c0b8de26c024d4febdfde69a9090b0508015
soumadip-dev <soumadipmajila@gmail.com>
2025-10-02 17:07:04 +0530

    feat: create basic AddHabitForm component

--- REDUX-TOOLKIT-LEARNING_2/src/App.tsx
+++ REDUX-TOOLKIT-LEARNING_2/src/App.tsx
@@ -1,14 +1,17 @@
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { Container, Typography } from '@mui/material';
+import AddHabitForm from './components/addHabitForm';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Container maxWidth="md">
-        <Typography variant="h1">Hello</Typography>
+        <Typography variant="h2" align="center">
+          Habit Tracker
+        </Typography>
+        <AddHabitForm />
       </Container>
-      <div>App</div>
     </Provider>
   );
 };
